77ac17bc052ed10b7383b6b0f718ce57e2cd7b1c
_config.yml app build.gradle gradle.properties gradlew gradlew.bat graphenej README.md ScreenShot settings.gradle zxingsupport <android_lgf@163.com>
2019-03-04 22:20:06 +0800

    add 联系客服UI

--- lib/components/personal/service/service_screent.dart
+++ lib/components/personal/service/service_screent.dart
@@ -26,10 +26,14 @@ class PersonalServiceScreen extends StatefulWidget {
 
 class _PersonalServiceScreenState extends BaseState<PersonalServiceScreen> {
   @override
+  void initState() {
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(236, 236, 236, 1),
-      appBar: buildCommonAppbar(report_title_text, onLeadTop: () {
+      appBar: buildCommonAppbar(service_title_text, onLeadTop: () {
         InputManageUtil.shutdownInputKeyboard();
         Navigator.pop(context);
       }),
@@ -42,6 +46,101 @@ class _PersonalServiceScreenState extends BaseState<PersonalServiceScreen> {
             color: Colors.white,
             borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(2.5), topRight: Radius.circular(2.5))),
+        child: Column(
+          children: <Widget>[_buildListTitle(), _buildListWidget(null)],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildListTitle() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      height: ScreenUtil().setHeight(35),
+      padding: EdgeInsets.only(left: ScreenUtil().setWidth(14)),
+      child: Row(
+        children: <Widget>[
+          Text(
+            service_floor_text,
+            style: TextStyle(
+                color: Color.fromRGBO(143, 143, 143, 1),
+                fontSize: ScreenUtil().setSp(12)),
+          ),
+          Padding(
+              padding: EdgeInsets.only(left: ScreenUtil().setWidth(50)),
+              child: Text(
+                service_name_text,
+                style: TextStyle(
+                    color: Color.fromRGBO(143, 143, 143, 1),
+                    fontSize: ScreenUtil().setSp(12)),
+              )),
+          Padding(
+              padding: EdgeInsets.only(left: ScreenUtil().setWidth(70)),
+              child: Text(
+                service_phone_text,
+                style: TextStyle(
+                    color: Color.fromRGBO(143, 143, 143, 1),
+                    fontSize: ScreenUtil().setSp(12)),
+              ))
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListWidget(List<dynamic> objects) {
+    return Expanded(
+        child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return _buildItemWidget(null, index);
+            }),
+        flex: 1);
+  }
+
+  Widget _buildItemWidget(object, index) {
+    return GestureDetector(
+      onTap: () {
+        print("item点击");
+      },
+      child: Container(
+        height: ScreenUtil().setHeight(30),
+        margin: EdgeInsets.only(
+            left: ScreenUtil().setWidth(8), right: ScreenUtil().setWidth(8)),
+        color: index % 2 == 0
+            ? Color.fromRGBO(245, 245, 245, 1)
+            : Color.fromRGBO(255, 255, 255, 1),
+        alignment: Alignment.centerLeft,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: ScreenUtil().setWidth(17)),
+              child: Text(
+                (index + 1).toString(),
+                style: TextStyle(
+                    color: Color.fromRGBO(46, 49, 56, 1),
+                    fontSize: ScreenUtil().setSp(12)),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: ScreenUtil().setWidth(58)),
+              child: Text(
+                "孙大圣",
+                style: TextStyle(
+                    color: Color.fromRGBO(46, 49, 56, 1),
+                    fontSize: ScreenUtil().setSp(12)),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: ScreenUtil().setWidth(83)),
+              child: Text(
+                "18598109872",
+                style: TextStyle(
+                    color: Color.fromRGBO(46, 49, 56, 1),
+                    fontSize: ScreenUtil().setSp(12)),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
